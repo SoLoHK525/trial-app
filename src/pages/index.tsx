a9dddad7afc41141447467352dce81103d04117b
Kevin <kevinhk0525@gmail.com>
2022-07-06 00:09:03 +0800

    Add material ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,22 @@
+import { Button } from '@mui/material';
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 function IndexPage() {
+    const navigate = useNavigate();
+
+    const navigateToAboutUs = () => {
+        navigate('/about');
+    }
+
     return (
-        <div>
-            <h1>
-                Hello, Welcome to my app!
-            </h1>
-            <Link to="/about">About us</Link>
-        </div>
-    )
+      <div>
+        <h1>Hello, Welcome to my app!</h1>
+        <Button variant="contained" onClick={navigateToAboutUs}>
+          About us
+        </Button>
+      </div>
+    );
 }
 
 export default IndexPage;
